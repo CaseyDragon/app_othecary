741da0175cede5375c0d38e71b8b2d5926a1c747
CaseyDragon <cndragon83@gmail.com>
2023-01-05 20:44:55 -0500

    recipes oils additives models created

--- appothecary/settings.py
+++ appothecary/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'appothecary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipemaker',
+        'USER': 'recipemakeruser',
+        'PASSWORD': 'recipemaker',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- recipemaker/models.py
+++ recipemaker/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Recipes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    recipename = models.CharField(max_length=100)
+    superfat = models.IntegerField()
+    NaOH = models.BooleanField()
+    lyeamount = models.DecimalField(max_digits=6, decimal_places=2)
+    lyeadditives = models.CharField(max_length=100)
+    oilname1 = models.CharField(max_length=100)
+    oilamount1 = models.DecimalField(max_digits=6, decimal_places=2)
+    oilname2 = models.CharField(max_length=100)
+    oilamount2 = models.DecimalField(max_digits=6, decimal_places=2)
+    oilname3 = models.CharField(max_length=100)
+    oilamount3 = models.DecimalField(max_digits=6, decimal_places=2)
+    oilname4 = models.CharField(max_length=100)
+    oilamount4 = models.DecimalField(max_digits=6, decimal_places=2)
+    oilname5 = models.CharField(max_length=100)
+    oilamount5 = models.DecimalField(max_digits=6, decimal_places=2)
+    additive = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name
+
+class Oils(models.Model):
+    name = models.CharField(max_length=100)
+    na_sap = models.DecimalField(max_digits=5, decimal_places=4)
+   
+    def __str__(self):
+        return self.name
+
+
+class Additives(models.Model):
+    name = models.CharField(max_length=100)
+    suggested = models.CharField(max_length = 100)
+    lyephase = models.BooleanField()
+
+    def __str__(self):
+        return self.name
