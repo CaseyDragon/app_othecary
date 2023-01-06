2217c802c5530445b6db211bab9efb8a4cc49dd0
CaseyDragon <cndragon83@gmail.com>
2023-01-05 20:52:35 -0500

    created superuser and created admin.py file

--- appothecary/settings.py
+++ appothecary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appothecary',
     'recipemaker',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- recipemaker/admin.py
+++ recipemaker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Oils, Additives, Recipes
 
 # Register your models here.
+admin.site.register(Oils)
+admin.site.register(Additives)
+admin.site.register(Recipes)
