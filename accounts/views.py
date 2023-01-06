ab5d611e8bca80cd5faa2f5d9bd8efe7eaae1b54
CaseyDragon <cndragon83@gmail.com>
2023-01-06 00:24:42 -0500

    accounts views created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 # Create your views here.
+
+
+def sign_up(response):
+    if response.method == 'POST':
+        form = SignUpForm(response.POST)
+        if form.is_valid():
+            form.save()
+            # user = form.save()
+            # login(response, user)
+            return redirect('/recipemaker/home')
+    else:
+        form = SignUpForm()
+    
+    return render(response, 'accounts/signup.html', {'form': form})
