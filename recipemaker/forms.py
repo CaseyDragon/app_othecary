2053c17565ff25958e52511129393163638d1dec
CaseyDragon <cndragon83@gmail.com>
2023-01-05 23:34:13 -0500

    create.html and views created

---
+++ recipemaker/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+class CreateNewRecipe(forms.Form):
+    recipename = forms.CharField(max_length=100)
+    superfat = forms.IntegerField()
+    lyeratio = forms.DecimalField(max_digits=6, decimal_places=2)
+    wateramount = forms.DecimalField(max_digits=6, decimal_places=2)
+    lyeadditives = forms.CharField(max_length=100)
+    oilname1 = forms.CharField(max_length=100)
+    oilamount1 = forms.DecimalField(max_digits=6, decimal_places=2)
+    oilname2 = forms.CharField(max_length=100)
+    oilamount2 = forms.DecimalField(max_digits=6, decimal_places=2)
+    oilname3 = forms.CharField(max_length=100)
+    oilamount3 = forms.DecimalField(max_digits=6, decimal_places=2)
+    oilname4 = forms.CharField(max_length=100)
+    oilamount4 = forms.DecimalField(max_digits=6, decimal_places=2)
+    oilname5 = forms.CharField(max_length=100)
+    oilamount5 = forms.DecimalField(max_digits=6, decimal_places=2)
+    additive = forms.CharField(max_length=100)
--- recipemaker/views.py
+++ recipemaker/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Oils, Additives, Recipes
+from .forms import CreateNewRecipe
 
-# Create your views here.
+def home(response):
+    return render(response, 'recipemaker/home.html', {})
+
+def create(response):
+    form = CreateNewRecipe()
+    return render(response, 'recipemaker/create.html', {})
+# @login_required(login_url='/registration/login')
+# def create(request):
+#     form = CreateNewRecipe()
+#     if request.method == 'POST':
+#         form = CreateNewRecipe(request.POST)
+#         if form.is_valid():
+#             # Recipes.user.objects.create(**form.cleaned_data)
+#             n = form.cleaned_data["recipename"]
+#             recipe = Recipes(name=n)
+#             # recipe.save()
+#             # request.user.recipes.create(recipe)
+#         return HttpResponseRedirect('/%i' %recipe.id)
+#     else:    
+#         form = CreateNewRecipe()
+#     return render(request, 'recipemaker/create.html', {'form':form})
+
+def oils_list(request):
+    oils = Oils.objects.all()
+    return render(request, 'recipemaker/oils_list.html', {'oils': oils})
+
+def oils_detail(request, id):
+    oil = Oils.objects.get(id=id)
+    return render(request, 'recipemaker/oils_detail.html', {'oil': oil})
+
+def additives_list(request):
+    additives= Additives.objects.all()
+    return render(request, 'recipemaker/additives_list.html', {'additives': additives})
+
+def additives_detail(request, id):
+    additive = Additives.objects.get(id=id)
+    return render(request, 'recipemaker/additives_detail.html', {'additive': additive})
+
+@login_required(login_url='/registration/login')
+def my_recipes(response):
+    recipes = Recipes.objects.all()
+        # if recipe in response.user.recipes.all():
+    return render(response, 'recipemaker/my_recipes.html', {'recipes': recipes})
+
+    
+
+@login_required(login_url='/registration/login')
+def recipe_detail(response, id):
+    recipe = Recipes.objects.get(id=id)
+   
+    # work out code for scaling here I believe
+    # if response.POST.get('save'):
+
+    # elif response.POST.get('scaleIt'):
+    #     pass    
+    return render(response, 'recipemaker/recipe_detail.html', {'recipe': recipe})
+
+def volume_calc(response):
+    return render(response, 'recipemaker/volume_calc.html', {})
+
+def basic_formulations(response):
+    return render(response, 'recipemaker/basic_formulations.html', {})
